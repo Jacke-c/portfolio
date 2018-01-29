70dc8a28576e61f4c18d9a4f914fce5d550500d1
Jakob Svensson <jakob@Jakobs-MacBook-Pro.local>
2018-01-29 16:36:10 +0100

    har testat massa

---
+++ wp-content/themes/portfolio/content-hero-top.php
@@ -0,0 +1,64 @@
+<div class="container">
+            
+                <div class="row">
+
+                        <div class="col-xs-12">
+                    
+                            <h1 class="visible-xs">H1 XS</h1>
+                            <h1 class="visible-sm">H1 SM</h1>
+                            <h1 class="visible-md">H1 MD</h1>
+                            <h1 class="visible-lg">H1 LG</h1>
+                            
+                        </div>
+
+                    </div>
+
+                    <div class="row">
+
+                        <div class="col-xs-6 col-lg-4">col-xs-6</div>
+                        <div class="col-xs-6 col-lg-8">col-xs-6</div>
+
+                    </div>
+
+                    <div class="row hidden-xs">
+
+                    <div class="col-sm-6">
+
+                        <div class="row">
+
+                            <div class="col-xs-4">col-xs-4</div>
+                            <div class="col-xs-8">col-xs-8</div>
+
+                        </div>
+                    </div>  
+                        
+                    <div class="col-sm-6">
+
+                        <div class="row">
+
+                            <div class="col-xs-4 col-md-push-8">col-xs-4</div>
+                            <div class="col-xs-8 col-md-pull-4">col-xs-8</div>
+
+                        </div>
+                    </div>
+                        
+
+                    </div>
+                 
+                    <div class="row footer">
+                        
+                        <div class="col-sm-4">col-sm-4</div>
+                        <div class="col-sm-4">col-sm-4</div>
+                        <div class="col-sm-4">col-sm-4</div>
+                        
+                    </div> 
+        
+        
+        <div class="col-md-4 col-xs-offset-8">
+        HEJ JU!
+        </div>
+
+                
+            
+        
+    </div>
---
+++ wp-content/themes/portfolio/template-start.php
@@ -0,0 +1,11 @@
+<?
+/*
+Template Name: Hem
+*/
+
+    get_header();
+
+    get_template_part('content', 'hero-top');
+
+    get_footer();
+?>
